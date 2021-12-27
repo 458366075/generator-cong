999fc11ef524618cf6678628e2fc198198505441
cong <458366075@qq.com>
2021-12-27 13:44:07 +0800

    init

---
+++ app/index.js
@@ -0,0 +1,36 @@
+const Generator = require('yeoman-generator')
+
+module.exports = class extends Generator {
+  prompting() {
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'projectName',
+        message: 'Your project name?',
+        default: this.appname,
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Your project author?',
+        default: 'cong',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Your project description?',
+      },
+    ]).then(answers => {
+      this.answers = answers
+    })
+  }
+  writing() {
+    const { projectName, gitUrl, keywords } = this.answers
+    const template = this.templatePath('pages-boilerplate')
+    const output = this.destinationPath(projectName)
+    this.fs.copyTpl(template, output, {
+      ...this.answers,
+      gitUrlReplace: gitUrl.replace('.git', ''),
+    })
+  }
+}
